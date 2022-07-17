38202de3ecf7a1c171119128662c48c98875644f
dabina2018 <35677021+dabina2018@users.noreply.github.com>
2022-07-17 13:13:06 -0400

    updating JS file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,8 @@
+// Nav hamburgerburger selections
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
 // scroll to top functionality
 const scrollUp = document.querySelector("#scroll-up");
 
@@ -13,22 +18,19 @@ scrollUp.addEventListener("click", () => {
   });
 });
 
-// Nav hamburgerburger selections
-const burger = document.querySelector("#burger-menu");
-const ul = document.querySelector("nav ul");
-const nav = document.querySelector("nav");
+//Hamburger menu function
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
+});
 
 burger.addEventListener("click", () => {
   ul.classList.toggle("show");
 });
 
 // Close hamburger menu when a link is clicked
-
-// Select nav links
-const navLink = document.querySelectorAll(".nav-link");
-
 navLink.forEach((link) =>
   link.addEventListener("click", () => {
     ul.classList.remove("show");
   })
 );
+
