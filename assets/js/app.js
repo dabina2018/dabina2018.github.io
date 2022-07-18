bf7b3214cfcff4917543e519f51756d719efaf72
dabina2018 <35677021+dabina2018@users.noreply.github.com>
2022-07-18 04:32:26 -0400

    adding code for form submisson

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,31 @@
+//firebase scripts
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCdVqLMM-LSmaqBSUC--X3jb8xC4QIpI50",
+    authDomain: "contact-form-53050.firebaseapp.com",
+    projectId: "contact-form-53050",
+    storageBucket: "contact-form-53050.appspot.com",
+    messagingSenderId: "839902421548",
+    appId: "1:839902421548:web:2e6c36d5b85714004ce789",
+    measurementId: "G-BY63JSYLK5"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
+
+//Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
 // Nav hamburgerburger selections
 const burger = document.querySelector("#burger-menu");
 const ul = document.querySelector("nav ul");
@@ -30,6 +58,38 @@ scrollUp.addEventListener("click", () => {
   });
 });
 
+/* form data */
 
+//Listen for form submit
+document.getElementById('contact').addEventListener('submit', submitForm);
+function submitForm(e){
+  e.preventDefault();
+
+  //Get user input values
+  var name = getInputVal('name');
+  var name = getInputVal('email');
+  var name = getInputVal('message');  
+
+  saveMessage(name, email, message);
+
+  console.log('Thank-you' + name + 'someone will contact you soon')
+  
+}
+
+//Function to grab form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+//save message to firebase
+function saveMessage(name, email, message){
+  var newMessageRef = messagesRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    email: email,
+    message: message
+  });
+
+}
 
 
